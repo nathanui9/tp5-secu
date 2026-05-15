b6b5dc1351eaf0e1a818de0e0e7ab08c7ea55892
nathanui9 <nmarcille@myges.fr>
2026-05-15 22:06:09 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,14 +1,15 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.14.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 6.34"
     }
+
     archive = {
       source  = "hashicorp/archive"
-      version = ">= 2.4.0"
+      version = "~> 2.7"
     }
   }
 }
